5a3e422f7fc0fef0ee0ac553abc8244a8675998c
dawxy <97687341@qq.com>
2017-12-11 21:54:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,8 +47,8 @@ sed -i "s|#\tfastcgi_pass 127.0.0.1:9000;|#\tfastcgi_pass 127.0.0.1:9000;\n\t}#a
 sed -i "s/post_max_size = 8M/post_max_size = 80M/g" /etc/php/7.0/fpm/php.ini
 sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 80M/g" /etc/php/7.0/fpm/php.ini
 
-/etc/init.d/php7.0-fpm restart
-service php7.0-fpm restart
+#/etc/init.d/php7.0-fpm restart
+#service php7.0-fpm restart
 
 cd src/core
 ./make.sh
